2e27ad75167acf4f39a69d2600b188edc8413d1f
Lauren Myers <lmmyers2@destop>
2022-03-02 09:48:59 -0700

    added instatiations to index and read

---
+++ api/authors/index.php
@@ -0,0 +1,46 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+}
+
+include_once '../../config/Database.php';
+include_once '../../models/Author.php';
+
+//Instantiate DB & connect
+
+$database = new Database();
+
+$db = $database->connect();
+
+
+switch ($method) {
+
+    case 'GET':
+
+        include_once 'read.php';
+        break;
+
+    case 'POST':
+        
+        //include_once 'create.php';
+        break;
+
+    case 'PUT':
+
+        //include_once 'update.php';
+        break;
+
+    case 'DELETE':
+
+        //include_once 'delete.php';
+
+    default:
+
+        echo "Request not GET POST PUT or DELETE.";
+
+}
---
+++ api/authors/read.php
@@ -0,0 +1,54 @@
+<?php
+
+//Instantiate author object
+
+$author = new Author($db);
+
+//read query
+
+$result = $post->read();
+
+//Get row count
+
+$num = $result->rowCount();
+
+//Check if any authors
+if($num > 0){
+
+    //Author array
+
+    $author_arr = array();
+
+    $author_arr['data'] = array();
+
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+
+        extract($row){
+
+            $author_item = array(
+
+                'id' => $id,
+                'author' => $author
+            );
+
+            //Push to "data"
+
+            array_push($author_arr['data'], $author_item);
+        }
+        
+        //Convert to JSON & output
+
+        echo json_encode($author_arr);
+
+    }
+
+
+}else{
+
+    //No authors
+
+    echo json_encode(
+        array('message' => 'No authors found');
+    )
+
+}
