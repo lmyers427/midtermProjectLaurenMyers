52f815d34a03d8588ef35e24ea9572c202d1e761
Lauren Myers <lmmyers2@destop>
2022-03-02 09:24:41 -0700

    Add folders and Author read.php

---
+++ models/Author.php
@@ -0,0 +1,44 @@
+<?php
+
+    class Author {
+
+        //DB stuff
+
+        private $conn;
+        private $table = 'authors';
+
+        //Author Properties
+        public $id;
+        public $name;
+
+
+        // Constructor with DB
+
+        public function __construct($db){
+
+            $this->conn = $db;
+        }
+
+        // Get Authors
+
+        public function read(){
+
+            //Create query
+            $query = 'SELECT a.id, 
+            a.author
+            FROM
+            ' . $this->table . 'a
+            ORDER BY
+                p.id';
+
+        //Prepare Statement
+
+        $stmt = $this->conn->prepare($query);
+
+        //Execute query
+        $stmt->execute();
+
+        return $stmt;
+            
+        }
+    }
