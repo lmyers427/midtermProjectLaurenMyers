d8e7863cb06e34d6afe4e043ba0ac75cc0df82f9
Lauren Myers <lmmyers2@destop>
2022-03-07 09:56:39 -0700

    Added index.php to root file

---
+++ api/index.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lauren Myers MidTerm</title>
+</head>
+<body>
+    <h1>Lauren Myers' Midterm Project</h1>
+
+</body>
+</html>
