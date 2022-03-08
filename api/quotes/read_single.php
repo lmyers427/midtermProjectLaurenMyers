37b55b3ea78f33ef6b84200005f7f1f28129f16f
Lauren Myers <lmmyers2@destop>
2022-03-08 08:25:01 -0700

    Update read_single.php quotes

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -5,7 +5,7 @@
 $quote = new Quote($db);
 
 //Get ID
-$quote->id = isset($_GET['id']) ? $_GET['id'] : die() ;
+$quote->id = isset($_GET['id']) ? $_GET['id'] : die();
        
 //Get Quote
 $quote->read_single();
