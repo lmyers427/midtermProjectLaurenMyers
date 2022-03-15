009299c6f99048e22ce3f3a0a0645da3caccbd5f
Lauren Myers <lmmyers2@destop>
2022-03-15 13:44:41 -0600

    Uncommented the Database for Heroku for testing

--- config/Database.php
+++ config/Database.php
@@ -3,17 +3,17 @@
 class Database{
     //Db Params
     
-    // private $host = 'tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-    // private $db_name = 'stl0wfgdrd8zcznq';
-    // private $username = 'uqnxczhpexp14jsl';
-    // private $password = null;
-    // private $conn;
-
-    private $host = 'localhost';
-    private $db_name = 'quotesdb';
-    private $username = 'root';
-    //private $password = null;
+    private $host = 'tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    private $db_name = 'stl0wfgdrd8zcznq';
+    private $username = 'uqnxczhpexp14jsl';
+    private $password = null;
     private $conn;
+
+    // private $host = 'localhost';
+    // private $db_name = 'quotesdb';
+    // private $username = 'root';
+    // //private $password = null;
+    // private $conn;
       
       
 
@@ -28,7 +28,7 @@ class Database{
         try {
 
             $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, 
-            $this->username); // $this->password = getenv('DB_PW'));
+            $this->username) $this->password = getenv('DB_PW'));
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         }catch(PDOException $e){
